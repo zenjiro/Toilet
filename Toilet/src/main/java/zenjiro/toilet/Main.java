c9cdd77ca1c0d9d27c650600d95de67a5f0518cf
zenjiro <zenjiro0123@gmail.com>
2015-01-10 17:03:59 +0900

    ダブルクリックで終了
    トレイアイコンをタスクトレイから削除するとメインスレッドが終了する。

--- Toilet/src/main/java/zenjiro/toilet/Main.java
+++ Toilet/src/main/java/zenjiro/toilet/Main.java
@@ -3,6 +3,8 @@ package zenjiro.toilet;
 import java.awt.AWTException;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -12,6 +14,13 @@ public class Main {
         final SystemTray tray = SystemTray.getSystemTray();
         final TrayIcon icon = new TrayIcon(ImageIO.read(Main.class.getResourceAsStream("/blue.jpg")));
         icon.setImageAutoSize(true);
+        icon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                System.out.println(event);
+                tray.remove(icon);
+            }
+        });
         tray.add(icon);
     }
 }
