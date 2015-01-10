9a4a7bcb25cc90ac1bdf608060fa0bb7f3337173
zenjiro <zenjiro0123@gmail.com>
2015-01-10 16:55:41 +0900

    タスクトレイアイコンを表示
    まだ終わらない。

--- Toilet/src/main/java/zenjiro/toilet/Main.java
+++ Toilet/src/main/java/zenjiro/toilet/Main.java
@@ -1,8 +1,17 @@
 package zenjiro.toilet;
 
+import java.awt.AWTException;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Main {
 
-    public static void main(final String[] args) {
-        System.out.println("こんにちは。");
+    public static void main(final String[] args) throws IOException, AWTException {
+        final SystemTray tray = SystemTray.getSystemTray();
+        final TrayIcon icon = new TrayIcon(ImageIO.read(Main.class.getResourceAsStream("/blue.jpg")));
+        icon.setImageAutoSize(true);
+        tray.add(icon);
     }
 }
